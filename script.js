2e59f9c9e4fcfdc6e73e4035b1ffea122f20b444
hendra495 <henra300807@gmail.com>
2024-08-14 10:30:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("jaegerButton").addEventListener("click", function() {
+    alert("Jaeger Activated! Prepare for battle!");
+    document.body.style.backgroundImage = "url('https://example.com/jaeger-activated.jpg')";
+});
+
